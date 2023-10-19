9a8e930f6355837a4ad0d5f2a672da0a40defc92
Lang Shimeng <robin@Langs-MacBook-Pro.local>
2023-10-20 08:46:14 +0900

    revise

--- memos/get_bq_columns.sql
+++ memos/get_bq_columns.sql
@@ -2,25 +2,17 @@ WITH
   view_name AS (
     SELECT 'your_project.your_dataset.your_view' AS view_name
   ),
-  view_definition AS (
-    SELECT
-      CONCAT(table_catalog, '.', table_schema, '.', table_name) AS view_name,
-      view_query
-    FROM
-      `your_project.your_dataset.INFORMATION_SCHEMA.VIEWS`
-  ),
   parsed_view AS (
     SELECT
-      view_name,
-      REGEXP_EXTRACT(view_query, r'FROM `([^`]+)`') AS data_source_table,
-      REGEXP_EXTRACT_ALL(view_query, r'SELECT ([^ ]+)') AS required_columns
+      CONCAT(table_catalog, '.', table_schema, '.', table_name) AS view_name,
+      view_query AS view_definition
     FROM
-      view_definition
-    WHERE view_name IN (SELECT view_name FROM view_name)
+      `your_project.your_dataset.INFORMATION_SCHEMA.VIEWS`
   )
 SELECT
   view_name,
-  data_source_table AS data_source_table_name,
-  ARRAY(SELECT column_name FROM UNNEST(required_columns) AS column_name) AS required_columns
+  REGEXP_EXTRACT(view_definition, r'FROM `([^`]+)`') AS data_source_table,
+  REGEXP_EXTRACT_ALL(view_definition, r'SELECT ([^ ]+)') AS required_columns
 FROM
-  parsed_view;
+  parsed_view
+WHERE view_name IN (SELECT view_name FROM view_name);
