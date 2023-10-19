7acdd338765b8c8424240b647f5fa95c603b8c48
Lang Shimeng <robin@Langs-MacBook-Pro.local>
2023-10-20 08:34:51 +0900

    revise sql

--- memos/get_bq_columns.sql
+++ memos/get_bq_columns.sql
@@ -1,19 +1,14 @@
 WITH
-  -- 指定要查询的视图名称
   view_name AS (
     SELECT 'your_project.your_dataset.your_view' AS view_name
   ),
-  -- 从INFORMATION_SCHEMA.VIEWS获取视图的定义
   view_definition AS (
     SELECT
-      view_name,
+      CONCAT(table_catalog, '.', table_schema, '.', table_name) AS view_name,
       view_query
     FROM
       `your_project.your_dataset.INFORMATION_SCHEMA.VIEWS`
-    WHERE
-      CONCAT(table_catalog, '.', table_schema, '.', table_name) IN (SELECT view_name FROM view_name)
   ),
-  -- 解析视图查询以获取数据源表和所需列
   parsed_view AS (
     SELECT
       view_name,
@@ -21,8 +16,8 @@ WITH
       REGEXP_EXTRACT_ALL(view_query, r'SELECT ([^ ]+)') AS required_columns
     FROM
       view_definition
+    WHERE view_name IN (SELECT view_name FROM view_name)
   )
--- 最终结果：数据源表和所需列
 SELECT
   view_name,
   data_source_table AS data_source_table_name,
