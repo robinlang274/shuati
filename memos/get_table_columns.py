ef1406b23a0da87b937d2abd8e343b80bc7cabb0
Lang Shimeng <robin@Langs-MacBook-Pro.local>
2023-10-20 09:57:41 +0900

    revise

--- memos/get_table_columns.py
+++ memos/get_table_columns.py
@@ -1,49 +1,19 @@
-# 从SQL文件中逐行读取内容
+from sql_metadata import Parser
+
+# 从SQL文件中读取内容
 def read_sql_file(file_path):
     with open(file_path, 'r') as sql_file:
-        lines = sql_file.readlines()
-    return lines
-
-# 提取视图需要的数据源表
-def extract_data_source_table(lines):
-    source_tables = []
-    in_from_clause = False
-    for line in lines:
-        if in_from_clause:
-            match = re.search(r'`([^`]+)`', line)
-            if match:
-                source_tables.append(match.group(1))
-            if ';' in line:
-                in_from_clause = False
-        if line.upper().startswith('FROM '):
-            in_from_clause = True
-    return source_tables
-
-# 提取视图需要的列名
-def extract_required_columns(lines):
-    in_select_clause = False
-    required_columns = []
-    for line in lines:
-        if in_select_clause:
-            line = line.strip()
-            if line.endswith(','):
-                line = line[:-1]
-            if not line.endswith(';'):
-                required_columns.append(line)
-            else:
-                in_select_clause = False
-        if line.upper().startswith('SELECT '):
-            in_select_clause = True
-            required_columns.append(line[len('SELECT '):])
-
-    return required_columns
+        sql_text = sql_file.read()
+    return sql_text
 
 if __name__ == "__main__":
     sql_file_path = 'your_sql_file.sql'  # 请替换为实际的SQL文件路径
 
-    lines = read_sql_file(sql_file_path)
-    data_source_tables = extract_data_source_table(lines)
-    required_columns = extract_required_columns(lines)
+    sql_text = read_sql_file(sql_file_path)
+
+    parser = Parser(sql_text)
+    data_source_tables = parser.tables
+    required_columns = parser.columns
 
     print("数据源表：", data_source_tables)
     print("所需列：", required_columns)
