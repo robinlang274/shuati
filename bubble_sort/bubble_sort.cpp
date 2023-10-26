4972988b578ba850ce952878114c65de1b15d809
Lang Shimeng <robin@Langs-MacBook-Pro.local>
2023-10-26 18:02:35 +0900

    buble sort

--- bubble_sort/bubble_sort.cpp
+++ bubble_sort/bubble_sort.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    int i,j;
+    int i,j,k;
     int a[100];
 
     for (i=0; i<8; i++){
@@ -10,6 +10,16 @@ int main(){
     }
 
     for (i=0; i<8; i++){
-        for (j=0; )
+        for (j=0; j<7-i; j++){
+            if (a[j] > a[j+1]){
+                k = a[j];
+                a[j] = a[j+1];
+                a[j+1] = k;
+            }
+        }
+    }
+
+    for (i=0; i<8; i++){
+        cout << a[i] << ' ';
     }
 }
